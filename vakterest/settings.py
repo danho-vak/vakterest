75efed1b363ed937450fc72081d870e54d78b36d
hovak <hovak@kakao.com>
2020-11-30 16:42:49 +0900

    static setting

--- vakterest/settings.py
+++ vakterest/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+#    'django.contrib.static',
     'django.contrib.staticfiles',
     'accountapp',
 ]
@@ -137,3 +138,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
