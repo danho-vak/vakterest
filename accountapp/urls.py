782f2ed1f33b4909ce3816700d46b457b9a8321a
hovak <hovak@kakao.com>
2020-12-06 11:35:59 +0900

    applied CBV for account

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 import accountapp.views
+
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('test/', accountapp.views.test, name='test')
+    path('test/', accountapp.views.test, name='test'),
+
+    path('create/', accountapp.views.AccountCreateView.as_view(), name='create'), # Class Base View를 적을 때 as_view()로
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 def test(request):
     return render(request, 'accountapp/temp.html')
+
+
+# 1. sign up
+# 2. sign in
+# 3. view info
+# 4. change info
+# 5. quit
+
+
+class AccountCreateView(CreateView): # generic view를 상속
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:test') # class형 view에서 사용하는 reverse()
+    template_name = 'accountapp/create.html'
