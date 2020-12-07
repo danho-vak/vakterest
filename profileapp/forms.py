08d62fd946560fe6122ec8ef9298354d7c51c787
danho-vak <hovak@kakao.com>
2020-12-07 17:57:49 +0900

    added profile creation

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -6,5 +6,5 @@ from profileapp.models import Profile
 class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'nickname', 'massage']
+        fields = ['image', 'nickname', 'message']
 
