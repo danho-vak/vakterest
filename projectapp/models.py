6f3eced5709d4419e89bbcb0e1dbff210e0b0538
danho-vak <hovak@kakao.com>
2020-12-09 17:34:48 +0900

    add project app

---
+++ projectapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    description = models.TextField(null=False)
+    image = models.ImageField(upload_to='project', null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
